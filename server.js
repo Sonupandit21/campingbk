aa1a6b1bf7770933cba5c90321d54beb731c5028
Sonupandit21 <113712075+Sonupandit21@users.noreply.github.com>
2026-03-18 21:12:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
